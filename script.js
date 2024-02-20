95e99378fdb6efc384213bf5e6af178f45bd2609
MCartwright1987 <matthewcartwright1987@yahoo.com>
2024-02-20 20:04:29 +0000

    testing

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener('scroll', function() {
     var scrollY = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
 
     // Adjust the translateY value to control the downward movement
-    var translateY = Math.min(190, scrollY * 0.6); // Adjust the factor as needed
+    var translateY = Math.min(190, scrollY * 0.61); // Adjust the factor as needed
   
     img.style.transform = 'translate(0px, ' + translateY + 'px)';
   });
