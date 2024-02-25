ea6f66b0c9ea42930dfd3c823651ab2038635bb3
MCartwright1987 <matthewcartwright1987@yahoo.com>
2024-02-25 15:11:33 +0000

    updated javascript for smoother parallax effect on mobile

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-//fetch the header html to inject into both pages
-// Use the Fetch API to get the content of the common HTML file
-fetch('header.html')
-  .then(response => response.text())
-  .then(data => {
-    // Inject the retrieved HTML content into the common-content div
-    document.getElementById('header').innerHTML = data;
-  })
-  .catch(error => console.error('Error fetching common content:', error))
+function fetchAndInjectHeader() {
+  fetch('header.html')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    })
+    .catch(error => console.error('Error fetching header content:', error));
+}
 
-// parallax effect
-window.addEventListener('scroll', function() {
-    var heroContainer = document.querySelector('.heroContainer');
-    var img = document.querySelector('.heroContainer img');
-  
-    var scrollY = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
+function addParallaxEffect() {
+  var heroContainer = document.querySelector('.heroContainer');
+  var img = document.querySelector('.heroContainer img');
+  var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-    // Adjust the translateY value to control the downward movement
-    var translateY = Math.min(160, scrollY * 0.6);
-  
-    img.style.transform = 'translate(0px, ' + translateY + 'px)';
+  // Use requestAnimationFrame for smoother scrolling
+  function updateParallax() {
+    if (window.scrollY === 0) {
+      img.style.transform = 'translateY(0)';
+    } else {
+      img.style.transform = 'translateY(' + Math.min(300, window.scrollY * 0.6) + 'px)';
+    }
+  }
+
+  // Add scroll event listener
+  window.addEventListener('scroll', function () {
+    // For mobile devices, use requestAnimationFrame
+    if (isMobile) {
+      requestAnimationFrame(updateParallax);
+    } else {
+      // For non-mobile devices, update directly
+      updateParallax();
+    }
   });
+}
+
+// Function to be executed when the DOM content is fully loaded
+function init() {
+  // Call functions to fetch header and add parallax effect
+  fetchAndInjectHeader();
+  addParallaxEffect();
+}
+
+// Event listener for DOMContentLoaded
+document.addEventListener('DOMContentLoaded', init);
